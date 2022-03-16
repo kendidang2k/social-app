a81d959003374d62682dabd643e1e95ab797ad6d
kendidang2k <kendidang04082k@gmail.com>
2022-03-16 20:36:39 +0700

    update src

--- components/Post.jsx
+++ components/Post.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function Post({ post }) {
   return (
-      <>
-        <p>{post.title}</p>  
-      </>
+    <>
+      <p>{post.title}</p>
+    </>
   )
 }
